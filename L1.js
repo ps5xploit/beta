1f969581a41caa271daf5377a76bf5b491fe3104
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-02-22 16:27:32 +0100

    Delete L1.js

--- L1.js
+++
@@ -1,7 +0,0 @@
-function manejarKeyPressShift(event) {
-    if (event.keyCode === 16) {
-        console.log("Tecla Shift funcionando"); // Mensaje de depuración
-        window.location.href = "https://www.youtube.com";
-    }
-}
-document.addEventListener("keydown", manejarKeyPressShift);
