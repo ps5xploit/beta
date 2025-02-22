f7cac8b0e0d2d2783247c2a81cc301b74b7c4d6b
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-02-22 16:17:17 +0100

    Add files via upload

--- L2.js
+++ L2.js
@@ -1,7 +1,11 @@
-function manejarKeyPressV(event) {
-    if (event.keyCode === 118) {
-        console.log("Tecla V funcionando"); // Mensaje de depuración
-        window.location.href = "https://www.google.com";
+// Función para manejar el evento de pulsación de tecla
+    function manejarKeyPress(event) {
+        // Verifica si la tecla presionada es la tecla Shift (código 16)
+        if (event.keyCode === 16) {
+            // Redirige directamente a google.com
+            window.location.href = "https://www.google.com";
+        }
     }
-}
-document.addEventListener("keydown", manejarKeyPressV);
+
+    // Registra un escuchador de eventos para keydown
+    document.addEventListener("keydown", manejarKeyPress);
